7cd84cab1e72eec62d11341fe25cfd4f7cf7cdee
Bryanjay Bodino <bryanjaybodino@gmail.com>
2024-08-10 15:34:03 +0800

    BNet.FTPServer.Sample

---
+++ BNet.FTPServer.Sample/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace BNet.FTPServer.Sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Start = new System.Windows.Forms.Button();
+            this.button_Stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_Status = new System.Windows.Forms.Label();
+            this.textBox_Hostname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_Start
+            // 
+            this.button_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Start.Location = new System.Drawing.Point(376, 189);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(163, 87);
+            this.button_Start.TabIndex = 0;
+            this.button_Start.Text = "Start";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // button_Stop
+            // 
+            this.button_Stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Stop.Location = new System.Drawing.Point(564, 189);
+            this.button_Stop.Name = "button_Stop";
+            this.button_Stop.Size = new System.Drawing.Size(163, 87);
+            this.button_Stop.TabIndex = 1;
+            this.button_Stop.Text = "Stop";
+            this.button_Stop.UseVisualStyleBackColor = true;
+            this.button_Stop.Click += new System.EventHandler(this.button_Stop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(372, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Status : ";
+            // 
+            // label_Status
+            // 
+            this.label_Status.AutoSize = true;
+            this.label_Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Status.Location = new System.Drawing.Point(471, 127);
+            this.label_Status.Name = "label_Status";
+            this.label_Status.Size = new System.Drawing.Size(57, 25);
+            this.label_Status.TabIndex = 3;
+            this.label_Status.Text = "Stop";
+            // 
+            // textBox_Hostname
+            // 
+            this.textBox_Hostname.Location = new System.Drawing.Point(46, 128);
+            this.textBox_Hostname.Multiline = true;
+            this.textBox_Hostname.Name = "textBox_Hostname";
+            this.textBox_Hostname.Size = new System.Drawing.Size(235, 206);
+            this.textBox_Hostname.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Hostname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(232, 46);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "FTP Server";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_Hostname);
+            this.Controls.Add(this.label_Status);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Stop);
+            this.Controls.Add(this.button_Start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Button button_Stop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_Status;
+        private System.Windows.Forms.TextBox textBox_Hostname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BNet.FTPServer.Sample/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BNet.FTPServer;
+
+
+namespace BNet.FTPServer.Sample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Commands commands = new Commands();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.OperationalStatus == OperationalStatus.Up)
+                {
+                    IPInterfaceProperties properties = ni.GetIPProperties();
+                    foreach (UnicastIPAddressInformation ip in properties.UnicastAddresses)
+                    {
+                        textBox_Hostname.AppendText("\r\nftp://" + ip.Address.ToString() + ":2022");
+                    }
+                }
+            }
+            //commands.UserCredentials.Add("bryan", "1234");
+            //commands.UserCredentials.Add("bodino", "143");
+            //commands.LoadCertificate("C:\\cert.pfx", "");
+        }
+        private async void button_Start_Click(object sender, EventArgs e)
+        {
+            label_Status.Text = "Running";
+            commands.Setup("C:\\", 2022);
+            await commands.StartAsync();
+        }
+
+        private async void button_Stop_Click(object sender, EventArgs e)
+        {
+            label_Status.Text = "Stop";
+            await commands.StopAsync();
+        }
+    }
+}
